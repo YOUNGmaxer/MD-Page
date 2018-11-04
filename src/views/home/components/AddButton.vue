33282ccf325041e4c21d230e9622b5c0bfa850df
YOUNGmaxer <517538470@qq.com>
2018-11-04 23:46:16 +0800

    添加文件按钮

--- src/views/home/components/AddButton.vue
+++ src/views/home/components/AddButton.vue
@@ -1,5 +1,9 @@
 <template>
-  
+<label for="add-file-btn">
+  <div class="md-addbtn">
+    <input type="file" name="md-file" id="add-file-btn"/>
+  </div>
+</label>
 </template>
 
 <script>
@@ -9,5 +13,5 @@ export default {
 </script>
 
 <style lang="scss">
-
+@import 'AddButton.scss';
 </style>
